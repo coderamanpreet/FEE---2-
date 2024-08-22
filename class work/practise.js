e9dcb38aa5fc899174039b44771876793a6ce994
coderamanpreet <sehgalamanpreet@yahoo.com>
2024-08-22 20:28:47 +0530

    Merge branch 'main' of https://github.com/coderamanpreet/FEE---2-

---
+++ class work/practise.js
@@ -0,0 +1,11 @@
+
+let n = prompt("Please enter your age:");
+
+n = Number(n);
+
+
+if (n >= 18) {
+  console.log("Yes, eligible for voting.");
+} else {
+  console.log("Not eligible for voting.");
+}
